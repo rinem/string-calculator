eaa4a8fd22983ad45e8a75a5011e7dcb0e7de8d7
Roshan Mishra <roshanmishrarinem@gmail.com>
2024-09-25 11:42:10 +0530

    feat: handling custom delimiters

--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -3,19 +3,28 @@ export function add(numbers: string): number {
     return 0;
   }
 
-  const numberArray = splitNumbers(numbers);
+  let delimiter = ",";
+  let numberPart = numbers;
+
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    delimiter = numbers.substring(2, delimiterEndIndex);
+    numberPart = numbers.substring(delimiterEndIndex + 1);
+  }
+
+  const numberArray = splitNumbers(numberPart, delimiter);
 
   return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-function splitNumbers(input: string): string[] {
+function splitNumbers(input: string, delimiter: string = ","): string[] {
   const result: string[] = [];
   let currentNumber = "";
 
   for (let i = 0; i < input.length; i++) {
     const char = input[i];
 
-    if (char === "," || char === "\n") {
+    if (char === delimiter || char === "," || char === "\n") {
       if (currentNumber !== "") {
         result.push(currentNumber);
         currentNumber = "";
