711691b4dd9bf7e188fd0ba62790a6c96869b9d5
Roshan Mishra <roshanmishrarinem@gmail.com>
2024-09-25 11:08:22 +0530

    feat: added logic for single number

--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,4 +1,8 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
+
+  const parsedNumber = parseInt(numbers);
+  if (!isNaN(parsedNumber)) return parsedNumber;
+
   return 0;
 }
