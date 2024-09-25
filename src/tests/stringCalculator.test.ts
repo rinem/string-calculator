c5c64e1cc1aca8f7946a94499c84d4ffc8aee3d7
Roshan Mishra <roshanmishrarinem@gmail.com>
2024-09-25 11:06:33 +0530

    feat: added add method signature

---
+++ src/tests/stringCalculator.test.ts
@@ -0,0 +1,7 @@
+import { add } from "../utils/stringCalculator";
+
+describe("String Calculator", () => {
+  test("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+});
